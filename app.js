2567e531ad9e6d2f73ede87d6c511721855ff356
llcseprado <91046919+llcseprado@users.noreply.github.com>
2022-10-04 18:24:28 +0800

    10/4

--- app.js
+++ app.js
@@ -23,12 +23,15 @@ app.engine("hbs", exphbs.engine({ extname: ".hbs" }));
 app.set("view engine", "hbs");
 
 //Router
-app.get("/", (req, res) => {
-  res.render("home");
-});
+// app.get("/", (req, res) => {
+//   res.render("home");
+// });
 
-app.get("/dtr", (req, res) => {
-  res.render("dtr");
-});
+const routes = require("./server/routes/user");
+app.use("/", routes);
+
+// app.get("/dtr", (req, res) => {
+//   res.render("dtr");
+// });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ server/routes/user.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const router = express.Router();
+const userController = require("../controllers/userController");
+
+// //Create, find, update, delete
+// router.get("/", userController.view);
+
+//get static files (css)
+app.use(express.static("public"));
+
+//Router
+router.get("/", (req, res) => {
+  res.render("home", {
+    title: "Homepage",
+    style: "dtr.css",
+  });
+});
+
+router.get("/dtr", (req, res) => {
+  res.render("dtr", {
+    title: "Daily Time Record",
+    style: "dtr.css",
+  });
+});
+
+router.get("/employees", (req, res) => {
+  res.render("employees-admin", {
+    title: "Employee Records",
+  });
+});
+
+module.exports = router;
